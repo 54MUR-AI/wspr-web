962ae0620db285324f0166413441a9afcb958f27
denalifox <denali.2.foxtrot@gmail.com>
2026-02-11 11:30:05 -0500

    Make delete buttons always visible with subtle opacity instead of group-hover

--- client/src/components/layout/WorkspaceSidebar.tsx
+++ client/src/components/layout/WorkspaceSidebar.tsx
@@ -60,9 +60,9 @@ export default function WorkspaceSidebar({ selectedWorkspace, onWorkspaceChange,
           </button>
           <button
             onClick={(e) => handleDeleteWorkspace(workspace.id, workspace.name, e)}
-            className="absolute -top-1 -right-1 w-4 h-4 bg-samurai-black border border-samurai-red rounded-full flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity"
+            className="absolute -top-1 -right-1 w-5 h-5 bg-samurai-black border border-samurai-red rounded-full flex items-center justify-center opacity-50 hover:opacity-100 transition-opacity z-10"
           >
-            <Trash2 className="w-2.5 h-2.5 text-samurai-red" />
+            <Trash2 className="w-3 h-3 text-samurai-red" />
           </button>
         </div>
       ))}
